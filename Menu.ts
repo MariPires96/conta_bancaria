192aa7ee1018e6fb98750f0e51c1c1ab5ac66d81
MariPires96 <mariana.pires96@hotmail.com>
2025-11-07 17:03:24 -0300

    Alterações Objetos e Classe

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,24 @@
 import readlineSync = require("readline-sync");
 import { colors } from './src/util/Colors';
+import { Conta } from './src/model/Conta';
 
 export function main() {
 
     let opcao: number;
+
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
+
+    const conta2: Conta = new Conta(2, 456 , 2, "Fernando", 100000);
+    conta2.visualizar();
+    conta2.sacar(12500);
+    conta2.visualizar();
+    conta2.depositar(4000);
+    conta2.visualizar();
 
     while (true) {
 
